520ad2a8c57767ec1c9d7a8a6e2e97d06a760a99
Adam Byrd <ajbyrd0723@gmail.com>
2019-10-15 15:43:48 -0500

    displaying foods html is up

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,8 +1,33 @@
 console.log("This is working and you will understand it")
 
+/* Creating DOM Element */
+
+const foodFactory = (food) => {
+    return `
+    <div class="foodList">
+    <h2>${food.name}</h2>
+    <section>${food.category}</section>
+    <aside>${food.ethnicity}</aside>
+    </div>
+    `
+}
+
+/* */
+
+const addFoodToDom = (foodString) => {
+    const foodListEl = document.querySelector(".foodList")
+    
+    foodListEl.innerHTML += foodString
+}
+
+/* Fetch call that passes  */
 
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food)
+            addFoodToDom(foodAsHTML)
+        })
     })
+
